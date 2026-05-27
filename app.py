de1c3c2ea47bae4f6a7eb838f4086472bd57fb4b
coordcompras-coder <coord.compras@caema.ma.gov.br>
2026-05-27 09:49:34 -0300

    10px

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ input[type="password"] {
 [data-testid="stMetricValue"] {
     color: #000000 !important;
     font-weight: 700 !important;
-    font-size: 20px !important;   /* VALOR PRINCIPAL */
+    font-size: 10px !important;   /* VALOR PRINCIPAL */
 }
 
 /* =========================================================
